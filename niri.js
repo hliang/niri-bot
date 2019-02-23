cd2ebf9b98162c21a04b6c155f2a77f97b677d49
hliang <hliang@github>
2019-02-22 23:24:28 -0600

    finish findConcert and findMovie

--- niri.js
+++ niri.js
@@ -1,33 +1,31 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-
 var NiriBot = require("./niriBot.js");
-
-// Create a new concert/song/movie object
-var bot = new NiriBot();
 
 // Grab search command line argument
 var search = process.argv[2];
 // Joining the remaining arguments since a query may contain spaces
 var term = process.argv.slice(3).join(" ");
 
+// Create a new concert/song/movie object
+var bot = new NiriBot();
 
 // Print whether searching for a song, movie or concert, print the term as well
 switch (search) {
     case "concert-this":
-        console.log("Searching for concert");
+        console.log("Searching for concert: " + term);
         bot.findConcert(term);
         break;
     case "spotify-this-song":
-        console.log("Searching for song");
+        console.log("Searching for song: " + term);
         bot.findSong(term);
         break;
     case "movie-this":
-        console.log("Searching for movie");
+        console.log("Searching for movie: " + term);
         bot.findMovie(term);
         break;
     case "do-what-it-says":
-        console.log("do commands in the txt file");
+        console.log("do commands in the txt file: " + term);
         bot.runCmdInFile(term);
         break;
     default:
---
+++ niriBot.js
@@ -0,0 +1,96 @@
+var axios = require("axios");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var moment = require('moment');
+
+var maxWidth = 79;
+
+// NiriBot constructor
+var NiriBot = function () {
+
+    // find concert
+    this.findConcert = function (artist) {
+        console.log("=".repeat(maxWidth))
+        // build url and get the data from bandsintown API
+        let url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        // limit items to print
+        let maxItem = 5;
+        axios.get(url)
+            .then(function (response) {
+                // not found
+                if (!response.data.isArray) {
+                    console.log("Oops, none found");
+                    console.log("=".repeat(maxWidth))
+                    return;
+                }
+                // select fileds and print to console
+                response.data.slice(0, maxItem).forEach(item => {
+                    concertData = {
+                        venue: item.venue.name,
+                        location: item.venue.city + ", " + item.venue.region,
+                        date: moment(item.datetime).format("YYYY/MM/DD"),
+                    }
+                    console.log(`${concertData.date}\t${concertData.venue}\t${concertData.location}`);
+                });
+                // more
+                if (response.data.length > maxItem) {
+                    console.log("... and " + (response.data.length - maxItem) + " more");
+                }
+                console.log("=".repeat(maxWidth))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+
+    // find song
+    this.findSong = function (term) {
+        console.log("found song: " + term);
+    };
+
+
+    // find movie
+    this.findMovie = function (movieName) {
+        // Run a request with axios to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryUrl)
+            .then(function (response) {
+                // error
+                if (response.data.Error) {
+                    console.log(response.data.Error);
+                    return;
+                }
+                // get rotten tomato rating
+                let tmtRaging = "";
+                for (let i=0; i < response.data.Ratings.length; i++) {
+                    console.log(response.data.Ratings[i]);
+                    if (response.data.Ratings[i].Source == "Rotten Tomatoes") {
+                        tmtRaging = response.data.Ratings[i].Value;
+                        break;
+                    }
+                }
+
+                movieData = ["Title: " + response.data.Title,
+                "Year: " + response.data.Year,
+                "Rating: " + response.data.imdbRating + "(IMDB), " + tmtRaging + "(Tomato)",
+                "Country: " + response.data.Country,
+                "Languate: " + response.data.Language,
+                "Plot: " + response.data.Plot,
+                "Actors: " + response.data.Actors,
+                ];
+                console.log(movieData.join("\n"));
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    // run commands in file
+    this.runCmdInFile = function (term) {
+        console.log("found cmd: " + term);
+    };
+}
+
+module.exports = NiriBot;
